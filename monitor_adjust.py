e582fd8207f432edc85829ed0825c12cafec91ac
littleyj <littleyj@21cn.com>
2017-02-21 11:11:27 +0800

    monitor af

--- monitor_adjust.py
+++ monitor_adjust.py
@@ -10,54 +10,58 @@ import urllib.parse
 import configparser
 
 class ProcessTransientFile(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(self,event):
+    def my_init(self):
+        self._data = []
+        self._insert_time = time.time()
+
+    def process_IN_MODIFY(self, event):
         line = file.readline()
         if line:
 #            print(line, end='')
-#            print(parseLog(line))
-            intodb(line)
+#            print(self.parseLog(line))
+            self.intodb(line)
 
-def intodb(line):
-    d = parseLog(line)
-    table = 'ods_adjust_log'
-    key = ['remote_addr','msec','log','click_id','app_id','app_name','app_version','store','tracker','tracker_name',
-'network_name','campaign','adgroup','creative','is_organic','gclid','rejection_reason','click_referer','adid','idfa',
-'android_id','idfa_android_id','idfv','gps_adid','referrer','user_agent','ip_address','activity_kind','click_time','click_time_hour',
-'conversion_duration','engagement_time','engagement_time_hour','install_time','install_time_hour','created_time','created_time_hour','reattributed_time','reattributed_time_hour','region',
-'country','city','language','device_name','device_type','os_name','sdk_version','os_version','timezone',
-'deeplink','label']
-    sql = "INSERT INTO " + table + "("
-    sql_value = ") VALUES("
-    value = []
-    for v in key:
-        sql += v + ","
-        sql_value += "%s,"
-        value.append(d.get(v))
-    sql = sql.strip(',') + sql_value.strip(',') + ')'
-#    print(sql)
-    try:
-        with conn.cursor() as cur:
-            cur.execute("SET NAMES utf8")
-            cur.execute(sql, value)
-        conn.commit()
-    except pymysql.OperationalError as e:
-        print('pymysql error: ' + str(e.args))
-#    finally:
-#        conn.close()
+    def intodb(self, line):
+        d = self.parseLog(line)
+        table = 'ods_adjust_log'
+        key = ['remote_addr','msec','log','click_id','app_id','app_name','app_version','store','tracker','tracker_name',
+            'network_name','campaign','adgroup','creative','is_organic','gclid','rejection_reason','click_referer','adid','idfa',
+            'android_id','idfa_android_id','idfv','gps_adid','referrer','user_agent','ip_address','activity_kind','click_time','click_time_hour',
+            'conversion_duration','engagement_time','engagement_time_hour','install_time','install_time_hour','created_time','created_time_hour','reattributed_time','reattributed_time_hour','region',
+            'country','city','language','device_name','device_type','os_name','sdk_version','os_version','timezone',
+            'deeplink','label']
+        sql = "INSERT INTO " + table + "(" + ','.join(key) + ') VALUES(' + ('%s,'*len(key)).strip(',') + ')'
+#        print(sql)
+        value = []
+        for v in key:
+            value.append(d.get(v))
+        self._data.append(value)
+        if self._insert_time != time.time() and self._insert_time + 5*60 <= int(time.time()) or len(self._data) >= 1000:
+            try:
+                with conn.cursor() as cur:
+                    cur.execute("SET NAMES utf8")
+                    cur.executemany(sql, self._data)
+                conn.commit()
+            except pymysql.OperationalError as e:
+                print('pymysql error: ' + str(e.args))
+#            finally:
+#                conn.close()
+            del self._data[:]
+            self._insert_time = time.time()
 
-def parseLog(line):
-    list = line.strip('\n').split('^A^R')[1:]
-    m = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(list[2][0:10])))
-    if list[1] == '{}':
-        d = dict(remote_addr=list[0], mesc=m)
-    else:
-        j = json.loads(urllib.parse.unquote(list[1]))
-        d = dict(remote_addr=list[0], msec=m, **j)
-    return d 
+    def parseLog(self, line):
+        list = line.strip('\n').split('^A^R')[1:]
+        m = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(list[2][0:10])))
+        if list[1] == '{}':
+            d = dict(remote_addr=list[0], mesc=m)
+        else:
+            j = json.loads(urllib.parse.unquote(list[1]))
+            d = dict(remote_addr=list[0], msec=m, **j)
+        return d
 
 def LogMonitor(path):
     print('job start!')
-    dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     wm = pyinotify.WatchManager()
     notifier = pyinotify.Notifier(wm)
     wm.watch_transient_file(path, pyinotify.IN_MODIFY, ProcessTransientFile)
@@ -65,7 +69,7 @@ def LogMonitor(path):
 
 if __name__ == '__main__':
     cf = configparser.ConfigParser()
-    cf.read(os.path.dirname(__file__) + '/../default.ini')
+    cf.read(os.path.abspath(os.path.dirname(__file__) + '/../default.ini'))
     db = {
         'host':cf.get('db', 'host'),
         'port':cf.getint('db', 'port'),
@@ -75,6 +79,8 @@ if __name__ == '__main__':
         'charset':cf.get('db', 'charset')
     }
     ngLog = cf.get('log', 'adjust')
+    data = []
+    insert_time = time.time()
     try:
         conn = pymysql.connect(**db)
     except pymysql.OperationalError as e:
--- monitor_appsflyer.py
+++ monitor_appsflyer.py
@@ -1,5 +1,5 @@
 #!/home/vagrant/script/ntom/bin/python
-from __future__ import print_function
+#from __future__ import print_function
 import pyinotify
 import time
 import os
@@ -7,54 +7,84 @@ import sys
 import pymysql
 import json
 import urllib.parse
+import configparser
 
 class ProcessTransientFile(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(self,event):
+    def my_init(self):
+        self._data = []
+        self._insert_time = time.time()
+
+    def process_IN_MODIFY(self, event):
         line = file.readline()
         if line:
 #            print(line, end='')
-#            print(parseLog(line))
-            intodb(line)
+#            print(self.parseLog(line))
+            self.intodb(line)
 
-def intodb(line):
-    d = parseLog(line)
-    try:
-        with conn.cursor() as cur:
-            cur.execute("SET NAMES utf8")
-            sql = "INSERT INTO appfly_channel(remote_addr,mesc,abc,dff) VALUES(%s,%s,%s,%s)"
-            cur.execute(sql, (d['remote_addr'], d['msec'], d.get('abc'), d.get('dff')))
-        conn.commit()
-    finally:
-        conn.close()
+    def intodb(self, line):
+        d = self.parseLog(line)
+        table = 'ods_appsflyer_log'
+        key = ['remote_addr', 'msec', 'clickid', 'site_id', 'device_ip', 'idfa', 'idfv', 'advertiser_id', 'android_id', 'app_name',
+               'app_id', 'install_time', 'click_time', 'wifi', 'campaign', 'country_code', 'city', 'language', 'device_brand','device_model',
+               'carrier', 'appsflyer_device_id', 'sdk_version', 'app_version_name', 'app_version', 'os_version', 'user_agent', 'event_name', 'event_time', 'event_value',
+               'monetary', 'currency', 'eventname', 'eventvalue']
+        sql = "INSERT INTO " + table + "(" + ','.join(key) + ') VALUES(' + ('%s,' * len(key)).strip(',') + ')'
+        print(sql)
+        print(d)
+        value = []
+        for v in key:
+            value.append(d.get(v))
+        self._data.append(value)
+        if self._insert_time != time.time() and self._insert_time + 5 * 60 <= int(time.time()) or len(self._data) >= 1:
+            try:
+                with conn.cursor() as cur:
+                    cur.execute("SET NAMES utf8")
+                    cur.executemany(sql, self._data)
+                conn.commit()
+            except pymysql.OperationalError as e:
+                print('pymysql error: ' + str(e.args))
+#            finally:
+#                conn.close()
+            del self._data[:]
+            self._insert_time = time.time()
 
-def parseLog(line):
-    list = line.strip('\n').split('^A^R')[1:]
-    m = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(list[2][0:10])))
-    if list[1] == '{}':
-        d = dict(remote_addr=list[0], mesc=m)
-    else:
-        j = json.loads(urllib.parse.unquote(list[1]))
-        d = dict(remote_addr=list[0], msec=m, **j)
-    return d 
+    def parseLog(self, line):
+        list = line.strip('\n').split('^A^R')[1:]
+        m = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(list[2][0:10])))
+        if list[1] == '{}':
+            d = dict(remote_addr=list[0], mesc=m)
+        else:
+            j = json.loads(urllib.parse.unquote(list[1]))
+            d = dict(remote_addr=list[0], msec=m, **j)
+        return d
 
 def LogMonitor(path):
+    print('job start!')
+    dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     wm = pyinotify.WatchManager()
     notifier = pyinotify.Notifier(wm)
     wm.watch_transient_file(path, pyinotify.IN_MODIFY, ProcessTransientFile)
-    notifier.loop()
+    notifier.loop(daemonize=False, pid_file=dir+'/adjust.pid', stdout=dir+'/log/run.log', stderr=dir+'/log/run_err.log')
 
 if __name__ == '__main__':
+    cf = configparser.ConfigParser()
+    cf.read(os.path.abspath(os.path.dirname(__file__) + '/../default.ini'))
     db = {
-        'host':'127.0.0.1',
-        'port':3306,
-        'user':'root',
-        'password':'',
-        'db':'test',
-        'charset':'utf8'
+        'host': cf.get('db', 'host'),
+        'port': cf.getint('db', 'port'),
+        'user': cf.get('db', 'user'),
+        'password': cf.get('db', 'password'),
+        'db': cf.get('db', 'db'),
+        'charset': cf.get('db', 'charset')
     }
-    conn = pymysql.connect(**db)
+    ngLog = cf.get('log', 'appsflyer')
+    data = []
+    insert_time = time.time()
+    try:
+        conn = pymysql.connect(**db)
+    except pymysql.OperationalError as e:
+        print('mysql connect error: ' + str(e.args))
 
-    ngLog = '/data/webservice/logs/nginx/growth/af_ad.log'
     file = open(ngLog, 'r')
     st_results = os.stat(ngLog)
     st_size = st_results[6]
